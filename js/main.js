caa9be38bde4cd383882836490bd9403b6183d9f
Alexander Kummer <AL_KM@ec4u.de>
2020-04-06 15:54:46 +0200

    fix datepicker init

--- js/main.js
+++ js/main.js
@@ -218,27 +218,7 @@
 
 	var goToTop = function() {
 		
-		$('.js-gotop').on('click', function(event){
-			
-			event.preventDefault();
-
-			$('html, body').animate({
-				scrollTop: $('html').offset().top
-			}, 500, 'easeInOutExpo');
-			
-			return false;
-		});
-
-		$(window).scroll(function(){
-
-			var $win = $(window);
-			if ($win.scrollTop() > 200) {
-				$('.js-top').addClass('active');
-			} else {
-				$('.js-top').removeClass('active');
-			}
-
-		});
+		
 	
 	};
 
@@ -305,3 +285,33 @@
 
 
 }());
+
+$( document ).ready(function() {
+	MsCrmMkt.MsCrmFormLoader.on("afterFormLoad", function(event) {
+	
+			$('#callback_date').datepicker();
+
+			$('.js-gotop').on('click', function(event){
+					
+				event.preventDefault();
+
+				$('html, body').animate({
+					scrollTop: $('html').offset().top
+				}, 500, 'easeInOutExpo');
+				
+				return false;
+			});
+
+			$(window).scroll(function(){
+
+				var $win = $(window);
+				if ($win.scrollTop() > 200) {
+					$('.js-top').addClass('active');
+				} else {
+					$('.js-top').removeClass('active');
+				}
+
+			});
+
+	});
+});
