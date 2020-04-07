7e31e48a412d2f57e6b4411e350a9ee2898befaf
Alexander Kummer <AL_KM@ec4u.de>
2020-04-07 11:50:11 +0200

    changes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,46 @@
 	
 	//'use strict';
 
+	var contentWayPoint = function() {
+		var i = 0;
+
+		// $('.gtco-section').waypoint( function( direction ) {
+
+
+			$('.animate-box').waypoint( function( direction ) {
+
+				if( direction === 'down' && !$(this.element).hasClass('animated-fast') ) {
+					
+					i++;
+
+					$(this.element).addClass('item-animate');
+					setTimeout(function(){
+
+						$('body .animate-box.item-animate').each(function(k){
+							var el = $(this);
+							setTimeout( function () {
+								var effect = el.data('animate-effect');
+								if ( effect === 'fadeIn') {
+									el.addClass('fadeIn animated-fast');
+								} else if ( effect === 'fadeInLeft') {
+									el.addClass('fadeInLeft animated-fast');
+								} else if ( effect === 'fadeInRight') {
+									el.addClass('fadeInRight animated-fast');
+								} else {
+									el.addClass('fadeInUp animated-fast');
+								}
+
+								el.removeClass('item-animate');
+							},  k * 200, 'easeInOutExpo' );
+						});
+						
+					}, 100);
+					
+				}
+
+			} , { offset: '85%' } );
+		// }, { offset: '90%'} );
+	};
 
 	// Loading page
 	var loaderPage = function() {
@@ -11,7 +51,8 @@
 	$(function(){
 		
 		loaderPage();
-
+		contentWayPoint();
+		
 	});
 
 
